9c5f70e7a6bfe495a5ba377279ecae197999d15f
jyfhmr <jyfhmr@gmail.com>
2025-06-09 15:52:33 -0400

    get route added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,4 +10,12 @@ export class AppController {
     console.log("Petición llegando a controlador:",body)
     return this.appService.searchAndBroadcastRates(body.currencySearched);
   }
+
+
+  @Get("/:currency")
+  returnCurrencyGet(@Param('currency') currency: string): Promise<string> {
+    console.log("Petición llegando a controlador:",currency)
+    return this.appService.searchAndBroadcastRates(currency);
+  }
+
 }
